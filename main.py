06d3cd1ee2ab7ec8f130525e9c18eccb70cc1845
kript0r3x <kript0r3x@gmail.com>
2023-05-28 14:29:37 -0400

    Able to retrieve mails, clean it and convert it to speech. But need to add more voices.

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from __future__ import print_function
-
+import requests
+from bs4 import BeautifulSoup
+from newspaper import Article
+import pyttsx3
 import os.path
+import base64
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
@@ -11,11 +15,17 @@ from googleapiclient.errors import HttpError
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
 
+engine = pyttsx3.init()
+
+# Set the properties for the speech
+engine.setProperty('rate', 180)  # Speed of speech (words per minute)
+engine.setProperty('volume', 0.8)
 
 def main():
     """Shows basic usage of the Gmail API.
     Lists the user's Gmail labels.
     """
+    count = 1
     creds = None
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
@@ -35,21 +45,64 @@ def main():
             token.write(creds.to_json())
 
     try:
+        query = f'from:morning@finshots.in'
         # Call the Gmail API
         service = build('gmail', 'v1', credentials=creds)
-        results = service.users().labels().list(userId='me').execute()
-        labels = results.get('labels', [])
 
-        if not labels:
-            print('No labels found.')
-            return
-        print('Labels:')
-        for label in labels:
-            print(label['name'])
+        results = service.users().messages().list(userId='me', q=query).execute()
+        messages = results.get('messages', [])
+        count = 2
+        for message in messages:
+            msg = service.users().messages().get(userId='me', id=message['id'], format='full').execute()
 
-    except HttpError as error:
-        # TODO(developer) - Handle errors from gmail API.
-        print(f'An error occurred: {error}')
+            # Extract subject and body from the message
+            payload = msg['payload']
+            headers = payload['headers']
+            subject = ''
+            body = ''
+
+            for header in headers:
+                if header['name'] == 'Subject':
+                    subject = header['value']
+
+            if 'parts' in payload:
+                parts = payload['parts']
+                for part in parts:
+                    if 'body' in part:
+                        data = part['body']
+                        if 'data' in data:
+                            body = data['data']
+                        elif 'attachmentId' in data:
+                            # If the body is an attachment, retrieve the attachment data
+                            attachment = service.users().messages().attachments().get(
+                                userId='me',
+                                messageId=message['id'],
+                                id=data['attachmentId']
+                            ).execute()
+                            body = attachment['data']
+
+            # Decode the body data
+            if body:
+                body = base64.urlsafe_b64decode(body).decode('utf-8')
+            count -= 1
+
+            soup = BeautifulSoup(body, "html.parser")
+            main_text = extract_main_text(str(soup))
+            print(main_text)
+            print("===============================================================================================================================================")
+            if count == 0:
+                break
+            engine.say(main_text)
+            engine.runAndWait()
+    except Exception as error:
+        print("Error %s" % error)
+
+
+def extract_main_text(html):
+    article = Article('')
+    article.set_html(html)
+    article.parse()
+    return article.text
 
 
 if __name__ == '__main__':
